e7b13b315df988d9e5b1b6e92f105d49101d48a9
kflau <nelson.kf.lau@gmail.com>
2018-10-05 18:46:45 +0800

    Update to handle Avaloq byte array response

--- src/main/java/com/agdelta/avaloq/jms/Receiver.java
+++ src/main/java/com/agdelta/avaloq/jms/Receiver.java
@@ -5,18 +5,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
+
 @Component
 public class Receiver {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
 
     @JmsListener(destination = "AMI_OUT", containerFactory = "myFactory")
-    public void receiveMessage(String content) {
-        LOGGER.info("Result from AMI_OUT: {}", content);
+    public void receiveMessage(byte[] content) throws UnsupportedEncodingException {
+        LOGGER.info("Result from AMI_OUT: {}", new String(content, "UTF-8"));
     }
 
     @JmsListener(destination = "AMI_SYNC_OUT", containerFactory = "myFactory")
-    public void receiveMessageSync(String content) {
-        LOGGER.info("Result from AMI_SYNC_OUT: {}", content);
+    public void receiveMessageSync(byte[] content) throws UnsupportedEncodingException {
+        LOGGER.info("Result from AMI_SYNC_OUT: {}", new String(content, "UTF-8"));
     }
 }
